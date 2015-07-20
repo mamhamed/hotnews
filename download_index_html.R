581b700abb936b595a0e3185193d0fa99290e531
Hamed Firooz <mhfirooz@yahoo.com>
2015-07-19 18:12:27 -0700

    remove the file to save some storage

--- download_index_html.R
+++ download_index_html.R
@@ -7,26 +7,34 @@ url = "http://dumps.wikimedia.org/other/pagecounts-raw/2015/2015-07/"
 
 doc = htmlTreeParse(paste(url,"index.html",sep=""), useInternalNodes = T)
 
-all_files <- xpathSApply(doc, "//a[contains(@href, 'pagecounts-2015')]", xmlValue)
+all_files <- sort(xpathSApply(doc, "//a[contains(@href, 'pagecounts-2015')]", xmlValue))
+
+calcTimeDiffPageCounts <- function(x){
+  (as.numeric(Sys.time()) - as.numeric(strptime(x, "pagecounts-%Y%m%d-%H0000.gz", tz="UTC")))/86400
+}
 
 all_local_files <- list.files(path=path.page.access.data)
 
-day_now <- as.integer(substr(as.character(Sys.Date()),start=9,stop=11))
+if (length(all_local_files) > 0){
+  removable_file <- all_local_files[1]
+}else{
+  removable_file = ""
+}
 
 for (pagecounts in all_files){
   print(pagecounts)
-  if (!pagecounts %in% all_local_files){
+  if ((!pagecounts %in% all_local_files) && (calcTimeDiffPageCounts(pagecounts) < DOWNLOAD_INTERVAL_IN_DAYS)){
     
     day <- as.integer(substr(pagecounts,start=18,stop=19))
     
-    if (day_now - day < DOWNLOAD_INTERVAL_IN_DAYS){
-       print(paste("Downloading ...",pagecounts))
-       local_filename = paste(path.page.access.data, pagecounts,sep="")
-      
-       download.file(url = paste(url,pagecounts,sep=""), 
-                  destfile = local_filename) 
-                  #method="wget", quiet = FALSE, mode = "w",
-                  #cacheOK = TRUE, extra = getOption("download.file.extra"))
-    }#download interval
+    print(paste("Downloading ...",pagecounts))
+     
+    local_filename = paste(path.page.access.data, pagecounts,sep="")
+    
+    download.file(url = paste(url,pagecounts,sep=""), 
+              destfile = local_filename) 
   }
 }
+
+print(paste("to save storage removing ", removable_file))
+file.remove(paste(path.page.access.data, removable_file,sep=""))
